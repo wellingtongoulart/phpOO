e3c35a3aba26b6699e961d84f87ba1c12e070226
wellingtongoulart <wellingtongoulart@outlook.com>
2021-12-31 19:11:42 -0300

    Novos atributos a classe

--- src/Contas.php
+++ src/Contas.php
@@ -2,39 +2,38 @@
 
 //propriedades privadas e métodos públicos
 //definir classe é prática de OO, sempre primeira maiscula - nela será definido os moldes que todo OBJETO/INSTANCIA conterá//
-class Conta
+class Conta //substantivo
 {
   private string $cpfTitular; //atributos da classe para o objeto
-  private string $nomeTitular; //depois de se tornar privado, o usuario não tem mais acesso
-  private float $saldo = 0;
+  private string $nomeTitular; //depois de se tornar privado, o usuario não tem mais acesso - encapsulamento
+  private float $saldo;
+  private static $numeroDeContas = 0; //atenção
   //dados
 
-  public function defineCpfTitular(string $cpf)
+  public function __construct(string $cpfTitular, string $nomeTitular) //metodo construct é conectado com new Conta()
   {
-    $this->cpfTitular = $cpf;
+    $this->cpfTitular = $cpfTitular;
+    $this->nomeTitular = $nomeTitular;
+    $this->validaNomeTitular($nomeTitular);
+    $this->saldo = 0;
+
+    self::$numeroDeContas++; //atenção //ou Conta
   }
 
-  public function recuperarCpfTitular(): string
+  public function recuperarCpfTitular(): string //get
   {
     return $this->cpfTitular;
   }
 
-  public function defineNomeTitular(string $nome)
-  {
-    $this->nomeTitular = $nome;
-  }
-
-  public function recuperarNomeTitular(): string
+  public function recuperarNomeTitular(): string //get
   {
     return $this->nomeTitular;
   }
 
-  public function recuperarSaldo(): float
+  public function recuperarSaldo(): float //get
   {
     return $this->saldo;
   }
-
-
 
   public function sacar(float $valorASacar): void
   {
@@ -63,32 +62,45 @@ class Conta
     $this->sacar($valorATransferir);
     $contaDestino->depositar($valorATransferir);
   }
+
+  private function validaNomeTitular(string $nomeTitular)
+  {
+    if (strlen($nomeTitular) < 6) {
+      echo "Favor inserir nome e sobrenome";
+      exit();
+    }
+  }
+
+  public static function recuperarNumeroDeContas(): int
+  {
+    return self::$numeroDeContas; //ou Conta
+  }
   //comportamentos
 }
 
 //As variáveis possuem um endereço para a classe, não possuem um valor inteiro, e por isso é possível ter mais que uma variável apontando para o mesmo lugar
 //Objeto
-$primeiraConta = new Conta();
-$primeiraConta->cpfTitular = '123.456.789-10';
-$primeiraConta->nomeTitular = 'Vinicius Viana';
-$primeiraConta->saldo = '5000';
+ /*     $primeiraConta = new Conta();
+      $primeiraConta->cpfTitular = '123.456.789-10';
+      $primeiraConta->nomeTitular = 'Vinicius Viana';
+      $primeiraConta->saldo = '5000';
 
-$segundaConta = new Conta();
-$segundaConta->cpfTitular = '987.654.321-41';
-$segundaConta->nomeTitular = 'Alan Araujo';
-$segundaConta->saldo = '200';
+      $segundaConta = new Conta();
+      $segundaConta->cpfTitular = '987.654.321-41';
+      $segundaConta->nomeTitular = 'Alan Araujo';
+      $segundaConta->saldo = '200';
 
-$teste = $segundaConta;
-$teste->saldo = 2000;
+      $teste = $segundaConta;
+      $teste->saldo = 2000;
 
-$segundaConta->depositar(-150);
+      $segundaConta->depositar(-150);
 
-$contaTeste = new Conta();
-$contaTeste->depositar(5080);
-$contaTeste2 = new Conta();
-$contaTeste->transferir(200, $contaTeste2);
+      $contaTeste = new Conta();
+      $contaTeste->depositar(5080);
+      $contaTeste2 = new Conta();
+      $contaTeste->transferir(200, $contaTeste2);
+não vou mais utilizar pela mudança na regra de negócio, mas fica o exemplo
 
-echo $contaTeste->saldo;
 
 /*
 exemplo de função criar conta:
